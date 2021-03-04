723b852fcc31d996361045db68157576e0dcd0f6
Christine Jade Avila <70692392+coederjade@users.noreply.github.com>
2021-03-04 16:11:53 +0800

    Add files via upload

---
+++ Python1stTry.py
@@ -0,0 +1,6 @@
+print("Welcome to Adventure Quest!")
+name = input("What's your name, kid? ")
+print("oh hey there,", name)
+age = input("How old are you? ")
+print("Whoah! cool, so you're already", age, "yrs old!")
+
